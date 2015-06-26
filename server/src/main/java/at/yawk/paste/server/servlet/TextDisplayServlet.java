eab5a45c81f0faf85cdabcec99a146e56e205f31
Jonas Konrad <me@yawk.at>
2015-06-26 21:14:50 +0200

    Syntax highlighting in text

--- server/src/main/java/at/yawk/paste/server/servlet/TextDisplayServlet.java
+++ server/src/main/java/at/yawk/paste/server/servlet/TextDisplayServlet.java
@@ -1,11 +1,13 @@
 package at.yawk.paste.server.servlet;
 
 import at.yawk.paste.model.Paste;
+import at.yawk.paste.model.PasteData;
 import at.yawk.paste.model.TextPasteData;
 import at.yawk.paste.server.PasteServlet;
 import at.yawk.paste.server.Request;
 import at.yawk.yarn.Component;
 import java.util.List;
+import lombok.Value;
 
 /**
  * @author yawkat
@@ -15,6 +17,11 @@ import java.util.List;
 public class TextDisplayServlet extends PasteServlet<TextPasteData> {
     @Override
     protected void handle(Request request, Paste paste, TextPasteData data, List<String> groups) throws Exception {
-        request.render("text", paste);
+        request.render("text", paste.withData(new SplittedText(data.getText().split("\n"))));
+    }
+
+    @Value
+    public static class SplittedText implements PasteData {
+        String[] lines;
     }
 }
--- shared/src/main/java/at/yawk/paste/model/Paste.java
+++ shared/src/main/java/at/yawk/paste/model/Paste.java
@@ -1,12 +1,19 @@
 package at.yawk.paste.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Wither;
 
 /**
  * @author yawkat
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Wither
 public class Paste {
     // serialize to _id for mongo
     @JsonProperty("_id") private String id;
