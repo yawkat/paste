a03bdff9b30ab10d457ef9fc180595528ecb3e08
Jonas Konrad <me@yawk.at>
2015-06-27 16:24:10 +0200

    Fix video display

--- server/src/main/java/at/yawk/paste/server/servlet/VideoDisplayServlet.java
+++ server/src/main/java/at/yawk/paste/server/servlet/VideoDisplayServlet.java
@@ -1,13 +1,13 @@
 package at.yawk.paste.server.servlet;
 
-import at.yawk.paste.model.SVGPasteData;
+import at.yawk.paste.model.VideoPasteData;
 import at.yawk.yarn.Component;
 
 /**
  * @author yawkat
  */
 @Component
-public class VideoDisplayServlet extends DisplayServlet<SVGPasteData> {
+public class VideoDisplayServlet extends DisplayServlet<VideoPasteData> {
     public VideoDisplayServlet() {
         super("video");
     }
